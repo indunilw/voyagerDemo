18b8bc45e5a900b52eed4b76e86eb22bb03e4751
Harry Manoharan <harry.manoharan@ifsworld.com>
2019-09-18 13:09:24 +0530

    changes

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -237,6 +237,27 @@ function MasterCtrl($scope, $cookieStore,$http) {
               window.location="/index.html";
             });
     }
+
+    var countDownDate = new Date("Oct 3, 2019 17:00:00").getTime();
+
+    var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+    document.getElementById("demo").innerHTML = "It's Time";
+  }
+}, 1000);
 }
 
 
